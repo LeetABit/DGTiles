906de7210b349d2161e80c9a2b62839c2fac078e
Hubert Bukowski <hubert.bukowski@gmail.com>
2021-10-29 00:16:18 +0200

    Improved debugging experience.

---
+++ src/components/VersionLabel.tsx
@@ -0,0 +1,32 @@
+//  Copyright (c) Hubert Bukowski. All rights reserved.
+//  Licensed under the MIT License.
+//  See LICENSE file in the project root for full license information.
+//
+//  @jsxImportSource @emotion/react
+
+import { Optional } from '../Types';
+import * as gitVersion from '../gitVersion.json';
+
+type VersionLabelProps = {
+    displayDate: Optional<boolean>
+}
+
+const versionString : string = gitVersion.version +
+    ((gitVersion.commitCount > 0) ? `-beta.${gitVersion.commitCount}` : '') +
+    ((gitVersion.isDirty) ? '-local' : '') +
+    `+Branch.${gitVersion.branch}` +
+    `+Sha.${gitVersion.sha}` +
+    `+Timestamp.${gitVersion.buildSafeTime}`;
+
+export const VersionLabel = ({ displayDate } : VersionLabelProps) =>
+    <>
+        <div>
+            <span>Version:</span>
+            <span>{versionString}</span>
+        </div>
+        {displayDate &&
+        <div>
+            <span>Date:</span>
+            <span>{gitVersion.buildTime}</span>
+        </div>}
+    </>
