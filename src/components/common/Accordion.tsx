1ccd701ab3aa34e0281ffda7483d20c105be7bbf
Hubert Bukowski <hubert.bukowski@gmail.com>
2022-10-02 18:22:20 +0200

    Arranged third-party licenses into accordion.

---
+++ src/components/common/Accordion.tsx
@@ -0,0 +1,84 @@
+//  Copyright (c) Hubert Bukowski. All rights reserved.
+//  Licensed under the MIT License.
+//  See LICENSE file in the project root for full license information.
+//
+//  @jsxImportSource @emotion/react
+
+import { CSSObject } from '@emotion/react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { mergeStyles } from 'src/styles/mergeStyles';
+import { ReactComponent as Wedge } from 'src/icons/Wedge.svg';
+
+interface Props {
+    label: React.ReactNode,
+    isExpanded?: boolean,
+    callbackSourceId?: string,
+    onIsExpandedChanged?: (sourceId: string | undefined) => void,
+}
+
+const containerStyle: CSSObject = {
+    width: 'calc(100% - 1em)',
+    display: 'inline-block',
+    margin: '0.5em',
+}
+
+const buttonStyle: CSSObject = {
+    cursor: 'pointer',
+}
+
+const contentStyle: CSSObject = {
+    marginLeft: '2em',
+}
+
+const collapsedWedgeStyle: CSSObject = {
+    width: '1em',
+    height: '1em',
+    verticalAlign: 'middle',
+};
+
+const expandedWedgeStyle: CSSObject = mergeStyles(collapsedWedgeStyle, {
+    rotate: '90deg',
+});
+
+export default function Accordion({ label, isExpanded, callbackSourceId, onIsExpandedChanged, children }: React.PropsWithChildren<Props>) {
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const [isExpandedState, setIsExpandedState] = useState<boolean>(false);
+    const effectiveIsExpanded = isExpanded ?? isExpandedState;
+
+    const onClickHandler = useCallback(() => {
+        if (onIsExpandedChanged) {
+            onIsExpandedChanged(callbackSourceId);
+        } else {
+            setIsExpandedState(!isExpandedState)
+        }
+    }, [isExpandedState, callbackSourceId, onIsExpandedChanged]);
+
+    const onKeyDownHandler = useCallback((e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === ' ' || e.code === 'Space' || e.key === 'Enter' || e.code === 'Enter') {
+            e.preventDefault();
+            onClickHandler();
+        }
+    }, [onClickHandler]);
+
+    useEffect(() => {
+        if (effectiveIsExpanded && containerRef.current) {
+            containerRef.current.scrollIntoView({ block: 'nearest' });
+        }
+    }, [effectiveIsExpanded]);
+
+    return (
+        <div ref={containerRef} css={containerStyle}>
+            <div role="button" tabIndex={0} onClick={onClickHandler} onKeyDown={onKeyDownHandler} css={buttonStyle}>
+                <Wedge css={effectiveIsExpanded ? expandedWedgeStyle : collapsedWedgeStyle} />
+                {label}
+            </div>
+            { effectiveIsExpanded
+                ? (
+                    <div css={contentStyle}>
+                        {children}
+                    </div>
+                )
+                : null }
+        </div>
+    );
+}
--- src/components/documents/ThirdPartyLicenses.tsx
+++ src/components/documents/ThirdPartyLicenses.tsx
@@ -4,25 +4,45 @@
 //
 //  @jsxImportSource @emotion/react
 
+import { useCallback, useState } from 'react';
 import thirdPartyLicenses from 'src/thirdPartyLicenses.g.json';
+import Accordion from '../common/Accordion';
 import MarkdownDocument from '../common/MarkdownDocument';
 
-const documentLines: string[] = [];
+const documents: Record<string, string> = {};
 thirdPartyLicenses.forEach(entry => {
-    documentLines.push(`# ${entry.packageOwner}`);
-    documentLines.push('');
+    const documentLines: string[] = [];
     entry.licenses.forEach(license => {
         documentLines.push(`**Packages**: ${Object.entries(license.packages).map(([packageName, packageUrl]) => `[${packageName}](${packageUrl})`).join(', ')}`);
         documentLines.push('');
         documentLines.push(license.licenseText.split('\n').map(line => line.trim()).map(line => `    ${line}`).join('\n'));
         documentLines.push('');
     });
+
+    documents[entry.packageOwner] = documentLines.join('\n');
 });
 
-const documentContent = documentLines.join('\n');
-
 export default function ThirdPartyLicenses() {
+    const [expandedAccordion, setExpandedAccordion] = useState<string | undefined>(undefined);
+    const onIsExpandedChangedHandler = useCallback((sourceId: string | undefined) => {
+        setExpandedAccordion(expandedAccordion === sourceId ? undefined : sourceId);
+    }, [expandedAccordion])
+
     return (
-        <MarkdownDocument content={documentContent} />
+        <>
+            {Object.entries(documents).map(([packageOwner, document]) => {
+                return (
+                    <Accordion
+                        label={packageOwner}
+                        isExpanded={packageOwner === expandedAccordion}
+                        callbackSourceId={packageOwner}
+                        onIsExpandedChanged={onIsExpandedChangedHandler}
+                        key={packageOwner}
+                    >
+                        <MarkdownDocument content={document} />
+                    </Accordion>
+                );
+            })}
+        </>
     );
 }
