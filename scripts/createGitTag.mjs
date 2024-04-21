2b3a6d87c812362dc5f29c93f8c1198505c10855
Hubert Bukowski <hubert.bukowski@gmail.com>
2024-04-21 21:04:53 +0200

    Allow manual build when tag already exists.

--- scripts/createGitTag.mjs
+++ scripts/createGitTag.mjs
@@ -3,11 +3,23 @@ import extractGitVersion from './extractGitVersion.mjs';
 export default async function createGitTag(github, context) {
     const gitVersion = await extractGitVersion();
 
-    const tagRef = `refs/tags/v${gitVersion.major}.${gitVersion.minor}.${gitVersion.patch}`;
-    await github.rest.git.createRef({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        ref: tagRef,
-        sha: context.sha,
-    });
+    const tag = `tags/v${gitVersion.major}.${gitVersion.minor}.${gitVersion.patch}`;
+    const tagRef = `refs/${tag}`;
+
+    try {
+        await github.rest.git.getRef({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            ref: tag,
+        });
+    } catch (error) {
+        if (error.status === 404) {
+            await github.rest.git.createRef({
+                owner: context.repo.owner,
+                repo: context.repo.repo,
+                ref: tagRef,
+                sha: context.sha,
+            });
+        }
+    }
 }
